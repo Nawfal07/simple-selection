4f4c670b59616b424d13b5daa6feeea4db5f5125
Nawfal Hamdi <nawfal.hamdi@cegedim-activ.com>
2022-03-08 16:22:21 +0100

    fix style

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const getFileContent = (e) => {
   const file = e.target.files[0];
-  console.log(e.target.files);
 
   const reader = new FileReader();
 
@@ -11,8 +10,6 @@ const getFileContent = (e) => {
 
 const setContentFromFile = (file) => {
   return (event) => {
-    console.log(file.name);
-    console.log(event.target.result);
     document.getElementById("content").innerHTML = event.target.result;
   };
 };
@@ -20,18 +17,19 @@ const setContentFromFile = (file) => {
 document.getElementById("file").addEventListener("change", getFileContent);
 
 function handleChangeColor() {
-  console.log("handle click");
   if (window.getSelection) {
-    const text =window.getSelection().toString();
+    const text = window.getSelection().toString();
     const selection = window.getSelection();
-    if (selection.rangeCount) {
+    if (
+      selection.rangeCount &&
+      selection.containsNode(document.getElementById("content"), true)
+    ) {
       const range = selection.getRangeAt(0);
       range.deleteContents();
       const span = document.createElement("span");
-      span.setAttribute("id","selectedText");
-      span.innerHTML=text;
+      span.setAttribute("id", "selectedText");
+      span.innerHTML = text;
       range.insertNode(span);
     }
-    console.log(text);
   }
 }
